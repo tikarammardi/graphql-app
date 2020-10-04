fcae7626b919bdc2ab0c3dc89b8b39528ebc1387
Tikaram Mardi <tikaram@datman.je>
2020-10-04 15:35:20 +0530

    test

--- schema/schema.js
+++ schema/schema.js
@@ -1,48 +1,48 @@
-const graphql = require('graphql')
-const {GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema} = graphql
-const axios = require('axios')
+const axios = require("axios");
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
 const CompanyType = new GraphQLObjectType({
-  name: 'Company',
+  name: "Company",
   fields: {
-    id: {type: GraphQLString},
-    name: {type: GraphQLString},
-    description: {type: GraphQLString},
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
   },
-})
+});
 
 const UserType = new GraphQLObjectType({
-  name: 'User',
+  name: "User",
   fields: {
-    id: {type: GraphQLString},
-    firstName: {type: GraphQLString},
-    age: {type: GraphQLInt},
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
     company: {
       type: CompanyType,
       async resolve(parentValue, args) {
         const res = await axios.get(
           `http://127.0.0.1:3000/company/${parentValue.id}`
-        )
-        return res.data
+        );
+        return res.data;
       },
     },
   },
-})
+});
 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
+  name: "RootQueryType",
   fields: {
     user: {
       type: UserType,
-      args: {id: {type: GraphQLString}},
+      args: { id: { type: GraphQLString } },
       async resolve(parentValue, args) {
-        const res = await axios.get(`http://127.0.0.1:3000/users/${args.id}`)
-        return res.data
+        const res = await axios.get(`http://127.0.0.1:3000/users/${args.id}`);
+        return res.data;
       },
     },
   },
-})
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
-})
+});
